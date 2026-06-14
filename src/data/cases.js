b3edeabb3d2699ddb55b0c9cdf7626027167b36f
Nidhiagr <nidhi.agrawal@jobtwine.com>
2026-06-14 22:00:12 +0530

    log 4 commit fix

--- src/data/cases.js
+++ src/data/cases.js
@@ -1,7 +1,7 @@
 import { blogVsBrain } from './cases/blog-vs-brain';
 import { whyPeopleLie } from './cases/coffee-vs-productivity';
 import { caseArchived } from './cases/myth-of-later';
-import { circuitBreaker } from './cases/circuitBreaker';
+import { circuitBreaker } from './cases/circuitbreaker';
 
 export const cases = [
   circuitBreaker,
